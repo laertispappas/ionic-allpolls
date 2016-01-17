c30e76055f6a9d4d265f9fec18106dd87a26d401
Laertis Pappas <laertis.pappas@gmail.com>
2016-01-17 04:06:03 +0200

    Dash public prototype

--- www/js/constants.js
+++ www/js/constants.js
@@ -3,6 +3,7 @@ angular.module('starter')
     notAuthenticated: 'auth-not-authenticated',
     notAuthorized: 'auth-not-authorized'
   })
+
   .constant('USER_ROLES', {
     admin: 'admin_role',
     public: 'public_role'
